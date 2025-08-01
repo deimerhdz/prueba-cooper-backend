4c45778e5a95a09f5a8eed7759ae37206396b145
deimerhdz <deimerhdz21@gmail.com>
2025-08-01 10:39:09 -0500

    Feat validando input del producto

--- src/producto/dto/create-producto.dto.ts
+++ src/producto/dto/create-producto.dto.ts
@@ -1,11 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsPositive } from 'class-validator';
 export class CreateProductoDto {
   @ApiProperty({ required: true })
+  @IsNotEmpty()
   nombre: string;
   @ApiProperty()
+  @IsOptional()
   descripcion?: string;
   @ApiProperty({ required: true })
+  @IsNotEmpty()
+  @IsPositive()
   precio: number;
   @ApiProperty({ required: true })
+  @IsNotEmpty()
   categoria: string;
 }
